cae7c1a4fe330cb7f8e948a2a3af32686c36f2b7
mritzmann <github@markus-mail.com>
2017-09-11 18:01:00 +0200

    add piwik

--- include/footer.php
+++ include/footer.php
@@ -8,3 +8,18 @@
 <script src="src/js/bootstrap.min.js"></script>
 <script src="src/js/bootstrap-table.min.js"></script>
 <script src="src/js/bootstrap-table-de-DE.min.js"></script>
+<!-- Piwik
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stat.dnszensur.ch/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+End Piwik Code -->
